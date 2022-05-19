d8fbe482c634204bba10ece9b529518d756a73d4
Razaera <razaera@pm.me>
2022-05-19 05:12:35 -0400

    Add clever-f package

---
+++ lua/packages/clever-f.lua
@@ -0,0 +1,13 @@
+local package = {
+    "rhysd/clever-f.vim",
+    event = "VimEnter",
+    after = "colorscheme",
+}
+
+function package.config()
+    vim.g.clever_f_across_no_line = 1
+    vim.g.clever_f_smart_case = 1
+    vim.g.clever_f_mark_direct = 1
+end
+
+return package
--- lua/packages/init.lua
+++ lua/packages/init.lua
@@ -24,6 +24,7 @@ function all_specs()
         spec("neo-tree"),
         spec("gruvbox"),
         spec("colorizer"),
+        spec("clever-f"),
         spec("telescope"),
     }
 end
