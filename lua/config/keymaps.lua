4a3b82098f7bf68a45346d65103bf3796120b53e
Razaera <razaera@pm.me>
2022-05-19 05:11:30 -0400

    Fix remaining keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,18 +1,48 @@
-local ne = { noremap = true, expr = true }
-local ns = { noremap = true, silent = true }
-local e = { expr = true }
+local which_key = require("which-key")
 
-function map(mode, keys, procedure, opts)
-    vim.keymap.set(mode, keys, procedure, ns)
+function ns(spec)
+    which_key.register(spec, { noremap = true, silent = true })
 end
 
-function nmap(keys, procedure, opts)
-    map("n", keys, procedure, opts)
+function t(str)
+    return vim.api.nvim_replace_termcodes(str, true, true, true)
 end
 
-function cmap(keys, procedure, opts)
-    map("c", keys, procedure, opts)
+function clear_search()
+    vim.api.nvim_feedkeys(t "<Plug>(clever-f-reset)", "x", true)
+    vim.cmd("nohlsearch")
 end
+
+ns({
+    -- Prevents the cursor from moving when joining lines
+    J = { "mzJ`z", "Join the below line into the current line" },
+    Y = { "y$", "Yank to the end of the current line" },
+    -- Keeps the cursor centered when moving to the next search result
+    n = { "nzzzv", "Go to next search result" },
+    -- Keeps the cursor centered when moving to the next search result
+    N = { "Nzzzv", "Go to previous search result" },
+
+    ["[a"] = { ":previous<CR>", "Go to the previous arg" },
+    ["]a"] = { ":next<CR>", "Go to the next arg" },
+    ["[A"] = { ":first<CR>", "Go to the first arg" },
+    ["]A"] = { ":last<CR>", "Go to the last arg" },
+    ["[b"] = { ":bprev<CR>", "Go to the previous buffer" },
+    ["]b"] = { ":bnext<CR>", "Go to the next buffer" },
+    ["[l"] = { ":lprevious<CR>", "Go to the previous loclist item" },
+    ["]l"] = { ":lnext<CR>", "Go to the next loclist item" },
+    ["[L"] = { ":lfirst<CR>", "Go to the first loclist item" },
+    ["]L"] = { ":llast<CR>", "Go to the last loclist item" },
+    ["[q"] = { ":cprevious<CR>", "Go to the previous quickfix list item" },
+    ["]q"] = { ":cnext<CR>", "Go to the next quickfix list item" },
+    ["[Q"] = { ":cfirst<CR>", "Go to the first quickfix list item" },
+    ["]Q"] = { ":clast<CR>", "Go to the last quickfix list item" },
+    ["[t"] = { ":tabprev<CR>", "Go to the previous tab" },
+    ["]t"] = { ":tabnext<CR>", "Go to the next tab" },
+
+    ["<ESC>"] = { clear_search, "Clear search highlights" },
+
+    ["<CR>"] = { "o<ESC>", "Insert a new line" },
+})
 
 for _, direction in pairs({"h", "j", "k", "l"}) do
     local subtbl = { direction = direction }
@@ -21,26 +51,9 @@ for _, direction in pairs({"h", "j", "k", "l"}) do
     -- local move_keys = string.gsub("<A-S-{direction}>", "{(%w+)}", subtbl)
     -- local move_cmd = string.gsub("<C-\\><C-n><C-w><S-{direction}>", "{(%w+)}", subtbl)
 
-    nmap(focus_keys, focus_cmd, ns)
+    vim.keymap.set("n", focus_keys, focus_cmd)
 end
 
---  nmap("k", [[(v:count >= 10 ? "m'" . v:count : "") . "k"]], ne)
---  nmap("j", [[(v:count >= 10 ? "m'" . v:count : "") . "j"]], ne)
-
-nmap("J", "mzJ`z", ns)
-
-nmap("n", "nzzzv", ns)
-nmap("N", "Nzzzv", ns)
-
-nmap("<CR>", "o<ESC>", ns)
-
-function expand_cwd()
-    if vim.fn.getcmdtype() == ":" then
-        return vim.fn.expand("%:h") .. "/"
-    else
-        return "%%"
-    end
-end
-
--- cmap("%%", expand_cwd, e)
--- vim.keymap.set("c", "%%", "getcmdtype() == ':' ? expand('%:h').'/' : '%%'", { expr = true })
+vim.keymap.set("c", "%%", [[getcmdtype() == ':' ? expand('%:h').'/' : '%%']], { expr = true })
+vim.keymap.set("n", "k", [[(v:count >= 5 ? "m'" . v:count : "") . "k"]], { expr = true, noremap = true })
+vim.keymap.set("n", "j", [[(v:count >= 5 ? "m'" . v:count : "") . "j"]], { expr = true, noremap = true })
